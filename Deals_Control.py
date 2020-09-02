3ba5b6cab8bd7eb504e3ee2e52b43d76238545b8
Pianist13 <pianist13r@gmail.com>
2020-09-02 17:20:15 +0300

    Версия 1.3.1, понятные названия переменных.

--- Deals_Control.py
+++ Deals_Control.py
@@ -1,29 +1,29 @@
 from openpyxl import load_workbook  # Импортируем всё необходимое.
 from openpyxl import Workbook
 
-compare_set_1 = set()  # Создаём изначальный список.
-compare_set_2 = set()  # Создаём список для сравнения.
-dbwb = load_workbook(filename='All_DEALS.xlsx')  # Загружаем текущую базу.
-dbws = dbwb['Лист1']
-for i in dbws['A']:  # Добавляем базу в изначальный список.
-    compare_set_1.add(i.value)
+compare_set_old = set()  # Создаём изначальный список.
+compare_set_new = set()  # Создаём список для сравнения.
+ad_wb = load_workbook(filename='All_DEALS.xlsx')  # Загружаем текущую базу.
+ad_ws = ad_wb['Лист1']
+for i in ad_ws['A']:  # Добавляем базу в изначальный список.
+    compare_set_old.add(i.value)
 
-b_swb = load_workbook(filename=r'C:\Users\Admin\Desktop\Источники данных\Аналитика 2.0\Битрикс.xlsx')  # Загружаем
+nd_wb = load_workbook(filename=r'C:\Users\Admin\Desktop\Источники данных\Аналитика 2.0\Битрикс.xlsx')  # Загружаем
 # свежий Битрикс.
-b_sws = b_swb['Битрикс']
-for i in b_sws['A']:  # Добавляем Битрикс в список для сравнения.
-    compare_set_2.add(i.value)
-compare_set_2.discard('ID')  # Убираем "ID".
+nd_ws = nd_wb['Битрикс']
+for i in nd_ws['A']:  # Добавляем Битрикс в список для сравнения.
+    compare_set_new.add(i.value)
+compare_set_new.discard('ID')  # Убираем "ID".
 
-for i in compare_set_2:  # Проверяем оставшиеся позиции второго списка на тип данных.
+for i in compare_set_new:  # Проверяем оставшиеся позиции второго списка на тип данных.
     if type(i) != int:
         i = int(i)
 
-lost_deals = compare_set_1 - compare_set_2  # Старая база - новая база = потеряшки.
+lost_deals = compare_set_old - compare_set_new  # Старая база - новая база = потеряшки.
 while None in lost_deals:
     lost_deals.remove(None)
 
-new_deals = compare_set_2 - compare_set_1 # Новая база - старые сделки = новые сделки.
+new_deals = compare_set_new - compare_set_old  # Новая база - старые сделки = новые сделки.
 while None in new_deals:
     new_deals.remove(None)
 
@@ -32,8 +32,8 @@ print()  # Разрыв строки.
 print('Потерянные сделки: ', len(lost_deals))  # Вывод количества потерянных сделок.
 
 for i in new_deals:  # Добавляем к основной базе новые сделки в колоночку.
-    dbws.append([i, ])
-dbwb.save('All_DEALS.xlsx')  # Сохраняем базу данных.
+    ad_ws.append([i, ])
+ad_wb.save('All_DEALS.xlsx')  # Сохраняем базу данных.
 
 lost_wb = Workbook()  # Создаём рабочую книгу под потеряшек.
 lost_ws = lost_wb.active
